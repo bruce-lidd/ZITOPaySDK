acc31c77ac0b03e7f43c953bea8f8450154167b2
lidongdong <964991296@qq.com>
2016-12-22 08:38:44 +0800

    提交版本号

--- Example/Pods/Target Support Files/ZITOPaySDK/ZITOPaySDK-umbrella.h
+++ Example/Pods/Target Support Files/ZITOPaySDK/ZITOPaySDK-umbrella.h
@@ -1,7 +1,40 @@
 #ifdef __OBJC__
 #import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
 #endif
 
+#import "ZITOPay.h"
+#import "ZITOBaseReq.h"
+#import "ZITOBaseResp.h"
+#import "ZITOPayReq.h"
+#import "ZITOPayResp.h"
+#import "ZITOPayAdapter.h"
+#import "ZITOPayRequest.h"
+#import "PaySandBoxViewController.h"
+#import "ZITOSandBoxAdapter.h"
+#import "NSDictionary+Utils.h"
+#import "NSString+IsValid.h"
+#import "ZITOCategory.h"
+#import "ZITOHTTPSessionManager.h"
+#import "ZITONetworking.h"
+#import "ZITONetworkReachabilityManager.h"
+#import "ZITOSecurityPolicy.h"
+#import "ZITOURLRequestSerialization.h"
+#import "ZITOURLResponseSerialization.h"
+#import "ZITOURLSessionManager.h"
+#import "ZITOPay+Utils.h"
+#import "ZITOPayCache.h"
+#import "ZITOPayConfig.h"
+#import "ZITOPayUtil.h"
+#import "ZITOPayAdapterProtocol.h"
+#import "ZITOPayObjects.h"
 
 FOUNDATION_EXPORT double ZITOPaySDKVersionNumber;
 FOUNDATION_EXPORT const unsigned char ZITOPaySDKVersionString[];
--- ZITOPaySDK.podspec
+++ ZITOPaySDK.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'ZITOPaySDK'
-  s.version          = ‘0.1.0'
+  s.version          = '0.1.0'
   s.summary          = 'A short description of ZITOPaySDK.'
 
 # This description is used to generate tags and improve search results.
